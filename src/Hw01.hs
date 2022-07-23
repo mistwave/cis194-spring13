21e78d73117eda2a627037fa5da0d1c4543e7008
Yifu Duan <duanyifu@hotmail.com>
2022-07-23 17:17:39 +0800

    hw01: complete tower of hanoi

--- src/Hw01.hs
+++ src/Hw01.hs
@@ -27,9 +27,36 @@ module Hw01 where
   sumDigits :: [Integer] -> Integer
   sumDigits [] = 0
   sumDigits xs = sumList (fmap (sumList . toDigits) xs)
-  
+
   -- exercise 4
   validate :: Integer -> Bool
   validate n = (sumDigits (doubleEveryOther (toDigits n))) `mod` 10 == 0
 
 
+  -- exercise 5
+  type Peg = String
+  type Move = (Peg, Peg)
+
+  -- move from the first peg to the *second*
+  hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
+  hanoi 0 _ _ _ = []
+  hanoi 1 a b _ = [(a, b)]
+  hanoi n a b c =
+    let step1 = hanoi (n-1) a c b
+        step2 = hanoi 1 a b "unused"
+        step3 = hanoi (n-1) c b a
+    in step1 ++ step2 ++ step3
+
+  -- exercise 6
+  -- move from the first peg to the *second*
+  hanoi4 :: Integer -> Peg -> Peg -> Peg -> Peg -> [Move]
+  hanoi4 0 _ _ _ _ = []
+  hanoi4 1 a b _ _ = [(a, b)]
+  hanoi4 2 a b c _ = hanoi 2 a b c
+  hanoi4 n a b c d =
+    let pile1 = n `div` 2
+        pile2 = n - pile1
+        step1 = hanoi4 pile1 a c b d
+        step2 = hanoi pile2 a b d
+        step3 = hanoi4 pile1 c b a d
+        in step1 ++ step2 ++ step3
