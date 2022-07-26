81a73415dc49976424573b3c6041adeb026f7378
Yifu Duan <duanyifu@hotmail.com>
2022-07-26 23:27:02 +0800

    hw01: add specs

--- test/Hw01Spec.hs
+++ test/Hw01Spec.hs
@@ -1,3 +1,13 @@
-module Hw01Spec where
+module Hw01Spec (spec) where
 
 import qualified Hw01 as SUT
+import Test.Hspec
+
+
+spec :: Spec
+spec = describe "execise for hw01" $ do 
+  it "validate credit card number correctly" $ do 
+    SUT.validate 12312312312123 `shouldBe` False
+    SUT.validate 4012888888881881 `shouldBe` True
+    SUT.validate 4012888888881882 `shouldBe` False
+  
