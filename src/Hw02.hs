788296aa0fb4a4db8db7b4ded590ae9e3e5e3e74
Yifu Duan <duanyifu@hotmail.com>
2022-07-29 21:54:16 +0800

    hw02: completed

--- src/Hw02.hs
+++ src/Hw02.hs
@@ -16,6 +16,9 @@ parseMessage line = case words line of
 parse :: String -> [LogMessage]
 parse txt = fmap parseMessage (lines txt)
 
+example1 :: IO [LogMessage]
+example1 = testParse parse 10 "./src/error.log"
+
 
 -- exercise 2
 insert :: LogMessage -> MessageTree -> MessageTree
@@ -24,3 +27,33 @@ insert new@(LogMessage _ newts _) (Node left old@(LogMessage _ oldts _) right) =
   if newts <= oldts then (Node (insert new left) old right)
   else (Node left old (insert new right))
 insert _ tree = tree
+
+
+-- exercise 3
+build :: [LogMessage] -> MessageTree
+build [] = Leaf
+build (x:xs) = insert x (build xs)
+
+
+-- exercise 4
+inOrder :: MessageTree -> [LogMessage]
+inOrder Leaf = []
+inOrder (Node left msg right) = (inOrder left) ++ [msg] ++ (inOrder right)
+
+
+-- exercise 5
+
+severe :: LogMessage -> Bool
+severe (LogMessage (Error x) _ _)
+  | x > 50 = True
+severe _ = False
+
+getMessage :: LogMessage -> String
+getMessage (LogMessage _ _ s) = s
+getMessage (Unknown s) = s
+
+whatWentWrong :: [LogMessage] -> [String]
+whatWentWrong lms = map getMessage (inOrder (build (filter severe lms)))
+
+example5 :: IO [String]
+example5 = testWhatWentWrong parse whatWentWrong "./src/sample.log"
